030a3c13681f3d57019a217edd458a104fa16c7c
Shreyash Hatkar <shreyash071003@gmail.com>
2024-06-28 16:21:18 +0530

    baseurl update

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
+      `https://wavspacesocialmedia-dn44ophfx-shreyashs-projects-49cbaa92.vercel.app//users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
