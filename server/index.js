eea20074717375517724269e08ad1dcba0e69642
Shreyash Hatkar <shreyash071003@gmail.com>
2024-06-28 16:28:09 +0530

    baseurl update

--- server/index.js
+++ server/index.js
@@ -29,7 +29,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://wave-space-socialmedia.vercel.app', // Explicitly allow your frontend origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization' // Specify allowed headers
+}));
+
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
